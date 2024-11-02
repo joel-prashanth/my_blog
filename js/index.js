1fb783e081343f48972eba355768d33ef446b1c0
Joel Prashanth <142143829+joel-prashanth@users.noreply.github.com>
2024-11-02 16:16:20 +0530

    Added JS folder to the project

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navbar = document.querySelector('.navbar');
+  
+    menuToggle.addEventListener('click', () => {
+      menuToggle.classList.toggle('active');
+      navbar.classList.toggle('active');
+    });
+  });
+  
